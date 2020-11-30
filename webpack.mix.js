21d2d946d89119c183368430bd3ebd058ca2da7f
kareemadesola <kareemadesola1999@gmail.com>
2020-11-30 11:58:14 +0100

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,33 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .extract(['vue'])
+    // .less('resources/less/app.less', 'public/css');
+    // .less('resources/less/app.less', 'public/stylesheets/styles.css');
+    // .sass('resources/sass/app.scss', 'public/css');
+    // .stylus('resources/stylus/app.styl', 'public/css');
+   /* .sass('resources/sass/app.sass', 'public/css', {
+    precision: 5
+});*/
+    /*.stylus('resources/stylus/app.styl', 'public/css', {
+    use: [
+        require('rupture')()
+    ]
+});*/
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        postCss: [
+            require('postcss-css-variables')()
+        ]
+    });
